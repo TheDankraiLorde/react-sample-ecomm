e4950b96a62c81f5f6d49a1e140d1879c0ca5bbb
Tulasi <tulasi.thummala.contractor@pepsico.com>
2019-08-09 08:23:06 -0500

    Added style

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './custom-button.styles.scss'
 
 const CustomButton = ({children, signInWithGoogle, ...otherProps}) => (
-    <button className='custom-button' onClick={signInWithGoogle}>
+    <button className={`custom-button ${signInWithGoogle?'google-sign-in':''}`} onClick={signInWithGoogle}>
         {children}
     </button>
 )
