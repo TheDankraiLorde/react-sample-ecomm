9ac6af7e513293422eef4ef4a48ad7a5ddb41984
Tulasi <tulasi.thummala.contractor@pepsico.com>
2019-08-09 01:12:07 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import Header from "./components/header/header.component"
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/shop" component={ShopPage}/>
-      </Switch>
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/shop" component={ShopPage}/>
+        </Switch>
+      </div>
     );
   }
 }
