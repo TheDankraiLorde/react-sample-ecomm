4fa94a3655134c31b57058aee128b0722176e3b4
TheDankraiLorde <menezesgaurav@gmail.com>
2019-08-12 14:33:11 +0530

    fixed label problem

--- src/App.js
+++ src/App.js
@@ -26,8 +26,6 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          }, () => {
-            console.log(this.state)
           });
         });
       }
