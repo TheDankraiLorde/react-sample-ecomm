420172063de595f37e0830d0ace1491cc12a2477
TheDankraiLorde <menezesgaurav@gmail.com>
2019-08-13 20:23:54 +0530

    Added React Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
 import {createStore,applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+
+import rootRed from './root-reducer'
+
+const middlewares = [logger];
+const store = createStore(rootRed, applyMiddleware(middlewares))
+
