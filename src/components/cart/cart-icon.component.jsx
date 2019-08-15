71031d9d77f2a79c8e2a63d3dd6dfb637c08c874
TheDankraiLorde <menezesgaurav@gmail.com>
2019-08-15 21:33:22 +0530

    added reselect and added cart

--- src/components/cart/cart-icon.component.jsx
+++ src/components/cart/cart-icon.component.jsx
@@ -4,10 +4,10 @@ import './cart-icon.styles.scss'
 import {connect} from 'react-redux'
 import {toggleCart} from '../../redux/cart/cart.actions'
 
-const CartIcon = ({toggleCart}) => (
+const CartIcon = ({toggleCart, itemCount}) => (
     <div className="cart-icon" onClick={toggleCart}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 
@@ -15,4 +15,10 @@ const mdtp = dispatch => ({
     toggleCart: () => dispatch(toggleCart())
 })
 
-export default connect(null,mdtp)(CartIcon)
+const mstp = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce(
+        (acc, cartItem) => (acc+cartItem.quantity)
+        , 0)
+})
+
+export default connect(mstp,mdtp)(CartIcon)
