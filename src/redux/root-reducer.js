d480fb3efcf1848d60e8fc1a7dfb7e33a99aeb70
Tulasi <tulasi.thummala.contractor@pepsico.com>
2019-08-20 08:37:27 -0500

    Added SCSS fixes and added Directory Selector

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,18 +1,20 @@
 import { combineReducers } from 'redux';
 import userRed from './user/user.reducer';
 import cartRed from './cart/cart.reducer';
+import directoryRed from './directory/directory.reducer'
 import storage from 'redux-persist/lib/storage';
 import {persistReducer} from 'redux-persist';
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['cart']
+    whitelist: ['cart','directory']
 }
 
 const rootReducer = combineReducers({
     user: userRed,
-    cart: cartRed
+    cart: cartRed,
+    directory: directoryRed
 })
 
 export default persistReducer(persistConfig,rootReducer)
