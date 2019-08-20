d57b95d3b938283ab98f2ce3f46d4c0d194a012a
Tulasi <tulasi.thummala.contractor@pepsico.com>
2019-08-20 04:43:11 -0500

    Added Redux Persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux';
 import userRed from './user/user.reducer';
 import cartRed from './cart/cart.reducer';
+import storage from 'redux-persist/lib/storage';
+import {persistReducer} from 'redux-persist';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userRed,
     cart: cartRed
 })
+
+export default persistReducer(persistConfig,rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import {createStore,applyMiddleware} from 'redux'
 import logger from 'redux-logger'
+import { persistStore } from 'redux-persist'
 
 import rootRed from './root-reducer'
 
 const middlewares = [logger];
-const store = createStore(rootRed, applyMiddleware(...middlewares))
+export const store = createStore(rootRed, applyMiddleware(...middlewares))
 
-export default store
+export const persistor = persistStore(store)
+export default {store,persistor}
